5b8de2ce7b0c3d84a4c8c2167c6d7390079ae663
인천 <icjang123@naver.com>
2022-01-24 17:13:58 +0900

    vue cdn 이용하여 바닐라 js 구조  프로젝트 변경

--- 2.vue/js/app.js
+++ 2.vue/js/app.js
@@ -1,6 +1,67 @@
+import SearchModel from '../js/models/SearchModel.js'
+import KeywordModel from '../js/models/KeywordModel.js'
+import HistoryModel from '../js/models/HistoryModel.js'
+
+
 new Vue({
     el: '#app',
     data: {
-        msg: 'hello world'
+        query: '',
+        searchResult: [],
+        submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
+        keywords: [],
+        history : []
+    },
+    created() {
+        this.selectedTab = this.tabs[0]
+        this.fetchKeyword()
+        this.fethcHistory()
+    },
+    methods: {
+        onSubmit(e) {            
+            this.search()
+        },
+
+        onKeyup() {
+            if(!this.query.length) this.onReset()
+        },
+        onReset() {
+            this.query = ''
+            this.searchResult = [];
+            this.submitted = false
+        },
+        onClickTab(tab) {
+            this.selectedTab =tab
+        },
+        onClickKeyword(keyword) {
+            this.query = keyword
+            this.search()  
+        },
+        onClickRemoveHistory(keyword) {
+            HistoryModel.remove(keyword)
+            this.fethcHistory()
+        },
+        fethcHistory() {
+            HistoryModel.list().then(data => {
+                this.history =data
+            })
+        },
+        fetchKeyword() {
+            KeywordModel.list().then(data => {
+                this.keywords =data
+            })
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted =true
+                this.searchResult = data
+            })
+
+            HistoryModel.add(this.query)
+            this.fethcHistory()
+        },
+        
     }
 })
